a8c504ac556f3b0d8ce1dd62e0bcff98e54aee8c
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-25 22:11:45 +0700

    tugas 24 js, Muhammad Rizwan

---
+++ tugas24.js
@@ -0,0 +1,13 @@
+/* Tugas 24 Javascript */
+// Muhammad Rizwan
+
+function tugasDuapuluhEmpat(){
+    var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+    console.log("Sebelumnya :",angka.toString());
+    console.log("Ascending :",angka.sort().toString());
+    console.log("Descending :",angka.reverse().toString());
+
+}
+
+tugasDuapuluhEmpat();
